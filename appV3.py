5bb90dbdc26d65c2b9d4e6e25b6b2982256c08ee
marta <20216610@life.hkbu.edu.hk>
2024-07-13 20:00:47 +0800

    seven

--- appV3.py
+++ appV3.py
@@ -11,14 +11,17 @@ st.set_page_config(page_title="मेरो ए.आई शिक्षक", page
 # Load secrets
 openai.api_key = st.secrets["OPENAI_API_KEY"]
 
-#Add custom CSS for hiding icons
-hide_github_icon="""
-#MainMenu {
-    visibility: hidden
-}
-"""
 
-st.markdown(hide_github_icon, unsafe_allow_html=True)
+
+st.markdown("""
+    <style>
+    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
+    .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
+    .viewerBadge_text__1JaDK {
+        display: none;
+    }
+    </style>
+    """, unsafe_allow_html=True)
 
 # Authentication setup
 with open('config.yaml') as file:
